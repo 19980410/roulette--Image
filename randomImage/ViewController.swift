31ef1394bb51423537ab0aece5ce276c22b9e2bb
19980410 <kkaoru1998@gmail.com>
2020-04-11 15:53:36 +0900

    Initial Commit

---
+++ randomImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  randomImage
+//
+//  Created by poti on 2020/04/11.
+//  Copyright © 2020 kaoru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
